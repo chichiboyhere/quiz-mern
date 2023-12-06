4cc5d641c323a7bd77c493837dd235e8381b011e
Tony <tonyjays@yahoo.com>
2023-12-06 04:03:25 +0100

    adjusted the route in api

--- routes/multiplicationResult.js
+++ routes/multiplicationResult.js
@@ -2,17 +2,16 @@ import express from "express";
 import { body } from "express-validator";
 
 import {getMultiplicationResults, postMultiplicationResult} from "../controllers/multiplicationResult.js";
-import { verifyUser } from "../utils/verifyToken.js";
+//import { verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 // GET /multiplicationResults/getResults
-router.get('/getResults/:id', verifyUser, getMultiplicationResults);
+router.get('/getResults/:id', getMultiplicationResults);
 
 // POST/multiplicationResult/postResult
 router.post(
   '/postResult',
-  verifyUser,
   [
     body('score'),
     body('questionCount'),
